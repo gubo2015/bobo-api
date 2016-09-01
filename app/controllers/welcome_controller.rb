aa10894f2393f5fafdb06385b558d61a57e4472a
gubo2015 <356112797@qq.com>
2016-09-01 18:19:12 +0800

    hhhhhhh

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
   def index
     # render json: {name: current_user.name}
-    render json: {name: "Hello World!" }
+    render json: {name: current_user.name }
   end
 end
